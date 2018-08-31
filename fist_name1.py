ebfd56a6740eee2d0703b48a7d9c6e5b5f0ddf61
wud23 <wud23@spdb.com.cn>
2018-08-31 16:10:25 +0800

    集成用户登录模块

--- fist_name1.py
+++ fist_name1.py
@@ -1,27 +1,125 @@
 import os
 import time
 from datetime import timedelta
-
+import functools
 from flask import Flask
 from flask import (
-    render_template, request, session
+    flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.utils import secure_filename
-
+from werkzeug.security import check_password_hash, generate_password_hash
 from bdpf.service import checkservice, Grouping
+import db
+from db import get_db
 
-# bp = Blueprint('auth', __name__, url_prefix='/auth')
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'upload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=5)
+app.config.from_mapping(
+    # store the database in the instance folder
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 basedir = os.path.abspath(os.path.dirname(__file__))
 ALLOWED_EXTENSIONS = set(['xlsx'])
+db.init_app(app)
 
 
-@app.route('/',methods=['GET'], strict_slashes=False)
+@app.route('/', methods=('GET', 'POST'))
+def login():
+    session.clear()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+        if not username:
+            error = '请输入用户名！'
+        elif not password:
+            error = '请输入密码！'
+        elif user is None:
+            error = '用户名错误！'
+        elif not check_password_hash(user['password'], password):
+            error = '密码错误！'
+
+        if error is None:
+            # store the user id in a new session and return to the index
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index_page'))
+
+        flash(error)
+
+    return render_template('login.html')
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@app.before_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = '请输入用户名！'
+        elif not password:
+            error = '请输入密码！'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = '用户 {0} 已存在！'.format(username)
+
+        if error is None:
+            # the name is available, store it in the database and go to
+            # the login page
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('login'))
+
+        flash(error)
+
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+
+@app.route('/index', methods=['GET'], strict_slashes=False)
 def index_page():
     return render_template('index.html')
 
@@ -62,124 +160,6 @@ def get_session():
     new_filename = session.get('new_filename')
     print(new_filename)
     return new_filename or u'no session'
-
-
-# 吴迪提供
-# def login_required(view):
-#     """View decorator that redirects anonymous users to the login page."""
-#
-#     @functools.wraps(view)
-#     def wrapped_view(**kwargs):
-#         if g.user is None:
-#             return redirect(url_for('auth.login'))
-#
-#         return view(**kwargs)
-#
-#     return wrapped_view
-#
-#
-# @bp.before_app_request
-# def load_logged_in_user():
-#     """If a user id is stored in the session, load the user object from
-#     the database into ``g.user``."""
-#     user_id = session.get('user_id')
-#
-#     if user_id is None:
-#         g.user = None
-#     else:
-#         g.user = get_db().execute(
-#             'SELECT * FROM user WHERE id = ?', (user_id,)
-#         ).fetchone()
-#
-#
-# @bp.route('/register', methods=('GET', 'POST'))
-# def register():
-#     """Register a new user.
-#
-#     Validates that the username is not already taken. Hashes the
-#     password for security.
-#     """
-#     session.clear()
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         db = get_db()
-#         error = None
-#
-#         if not username:
-#             error = '请输入用户名！'
-#         elif not password:
-#             error = '请输入密码！'
-#         elif db.execute(
-#                 'SELECT id FROM user WHERE username = ?', (username,)
-#         ).fetchone() is not None:
-#             error = '用户 {0} 已存在！'.format(username)
-#
-#         if error is None:
-#             # the name is available, store it in the database and go to
-#             # the login page
-#             db.execute(
-#                 'INSERT INTO user (username, password) VALUES (?, ?)',
-#                 (username, generate_password_hash(password))
-#             )
-#             db.commit()
-#             return redirect(url_for('auth.login'))
-#
-#         flash(error)
-#
-#     return render_template('auth/register.html')
-#
-#
-# @bp.route('/login', methods=('GET', 'POST'))
-# def login():
-#     """Log in a registered user by adding the user id to the session."""
-#     session.clear()
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         db = get_db()
-#         error = None
-#         user = db.execute(
-#             'SELECT * FROM user WHERE username = ?', (username,)
-#         ).fetchone()
-#         if not username:
-#             error = '请输入用户名！'
-#         elif not password:
-#             error = '请输入密码！'
-#         elif user is None:
-#             error = '用户名错误！'
-#         elif not check_password_hash(user['password'], password):
-#             error = '密码错误！'
-#
-#         if error is None:
-#             # store the user id in a new session and return to the index
-#             session.clear()
-#             session['user_id'] = user['id']
-#             return redirect(url_for('auth.index'))
-#
-#         flash(error)
-#
-#     return render_template('auth/login.html')
-#
-#
-# @bp.route('/logout')
-# def logout():
-#     """Clear the current session, including the stored user id."""
-#     session.clear()
-#     return redirect(url_for('auth.login'))
-#
-#
-# @bp.route('/index')
-# def index():
-#     """Show all the posts, most recent first."""
-#     db = get_db()
-#     posts = db.execute(
-#         'SELECT p.id, title, body, created, author_id, username'
-#         ' FROM post p JOIN user u ON p.author_id = u.id'
-#         ' ORDER BY created DESC'
-#     ).fetchall()
-#     return render_template('auth/index.html', posts=posts)
-# end wudi
 
 
 if __name__ == '__main__':
